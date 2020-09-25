b5e68b62f08e8f5c69d25a02c8485d68216b7913
sales <sales@angularadvisors.com>
2020-09-25 10:34:13 -0600

    Added getToken

--- SPA/src/app/app.component.ts
+++ SPA/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spa';
+
+  constructor(private http: HttpClient) { }
+
+  public getToken(event: Event) {
+    const headers = { 'Content-Type': 'application/x-www-form-urlencoded'}
+    const body = { title:'client_id=credentials_client_id&client_secret=C1ientCredentials_$ecret&grant_type=client_credentials' }
+    this.http.post<any>('https://localhost:44342/connect/token/', body, { headers }).subscribe(response => {
+            console.log(response);
+    })        
+  }
+     
 }
